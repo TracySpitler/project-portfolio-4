0302562331e494c36d051bfae9e9cdc5b066e7dd
Tracy Spitler <tracyspitler@gmail.com>
2018-09-08 17:33:24 -0400

    Updated App.js to fetch JSON data from The Movie DB

--- src/App.js
+++ src/App.js
@@ -2,20 +2,77 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var url = "https://api.themoviedb.org/3/movie/popular?api_key=2cb547f6b21d9fecbf9c3ada7fa19ba3"
+
+// fetch(url)
+//     .then(response => {
+//     if (response.ok) {
+//         return response.json()
+//     } else {
+//         return Promise.reject('something went wrong!')
+//     }
+// })
+// .then(data => {
+//     //JS
+//     console.log('data is ', data)
+// })
+// .catch(error => console.log('error is ', error))
+
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: [],
+            isLoaded: false,
+        }
+    }
+
+    componentDidMount() {
+        fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                return Promise.reject('something went wrong!')
+            }
+        })
+        .then(data => {
+            console.log('data is ', data)
+
+            this.setState({
+                isLoaded: true,
+                movies: data.results,
+            })
+        })
+        .catch(error => console.log('error is ', error))
+    }
+
+    render() {
+
+        return (
+            <div className="App">
+            <p>"Popular Movies"</p>
+            {this.state.movies.map((movie, id) => (
+                <p key={id}>
+                    ID: {movie.id}<br />
+                    Title: {movie.title}<br />
+                    Overview: {movie.overview}
+                </p>
+            ))}
+            {
+                // <header className="App-header">
+                //   <img src={logo} className="App-logo" alt="logo" />
+                //   <h1 className="App-title">Welcome to React</h1>
+                // </header>
+                // <p className="App-intro">
+                //   To get started, edit <code>src/App.js</code> and save to reload.
+                // </p>
+            }
+            </div>
+        );
+    }
 }
 
 export default App;
